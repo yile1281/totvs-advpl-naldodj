ddc20e8b77ede7be5785f1d700734dcb75bc0922
marinaldo.jesus <marinaldo.jesus@91259c0f-3c9b-aaa7-74a1-b0048a0373d4>
2013-05-08 00:07:29 +0000

    new executables from harbour Nightly builds

---
+++ tBigNumber/compileAll.bat
@@ -0,0 +1,7 @@
+@echo off
+echo BATCH FILE FOR Harbour tBigNumber
+rem ============================================================================
+
+call compile.bat
+call compile64.bat
+call compilearm.bat

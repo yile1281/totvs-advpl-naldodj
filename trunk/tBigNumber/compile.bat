ef86889fa481ea67863c369e515753f45f8ec298
marinaldo.jesus <marinaldo.jesus@91259c0f-3c9b-aaa7-74a1-b0048a0373d4>
2013-07-31 21:19:07 +0000

    fix little bugs

--- trunk/tBigNumber/compile.bat
+++ trunk/tBigNumber/compile.bat
@@ -2,19 +2,59 @@
 echo BATCH FILE FOR Harbour MinGW32
 rem ============================================================================
 SET _PATH=%PATH%
-	c:\hb32\bin\hbmk2.exe -cpp tBigNtst_array.hbp
-	c:\hb32\bin\upx.exe   tBigNtst_array.exe
-	c:\hb32\bin\hbmk2.exe -cpp tBigNtst_array_mt.hbp
-	c:\hb32\bin\upx.exe   tBigNtst_array_mt.exe
-	c:\hb32\bin\hbmk2.exe -cpp tBigNtst_dbfile.hbp
-	c:\hb32\bin\upx.exe   tBigNtst_dbfile.exe
-	c:\hb32\bin\hbmk2.exe -cpp tBigNtst_dbfile_memio.hbp
-	c:\hb32\bin\upx.exe   tBigNtst_dbfile_memio.exe
-	c:\hb32\bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_mt.hbp
-	c:\hb32\bin\upx.exe   tBigNtst_dbfile_memio_mt.exe
-	c:\hb32\bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_mt_dyn_obj.hbp
-	c:\hb32\bin\upx.exe   tBigNtst_dbfile_memio_mt_dyn_obj.exe
-	move /Y *.exe exe\mingw32 
+SET _HB_PATH=%HB_PATH%
+SET HB_PATH=c:\hb32\
+
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_array.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_array.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_array_assignv.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_array_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_array_mt.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_array_mt.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_array_mt_assignv.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_array_mt_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_assignv.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_assignv.exe
+
+	%HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_mt.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_mt.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_mt_assignv.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_mt_assignv.exe
+		
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_memio.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_assignv.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_dyn_obj.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_dyn_obj.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_dyn_obj_assignv.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_dyn_obj_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_mt.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_mt.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_mt_assignv.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_mt_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_mt_dyn_obj.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_mt_dyn_obj.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp tBigNtst_dbfile_memio_mt_dyn_obj_assignv.hbp
+    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_mt_dyn_obj_assignv.exe
+	
+    move /Y *.exe exe\mingw32 
+SET HB_PATH=%_HB_PATH%
 SET PATH=%_PATH%
 
 
--- trunk/tBigNumber/compile64.bat
+++ trunk/tBigNumber/compile64.bat
@@ -2,13 +2,59 @@
 echo BATCH FILE FOR Harbour MinGW64
 rem ============================================================================
 SET _PATH=%PATH%
-	c:\hb32\bin\hbmk2.exe tBigNtst_array.hbp -comp=mingw64 -cpp
-	c:\hb32\bin\hbmk2.exe tBigNtst_array_mt.hbp -comp=mingw64 -cpp
-	c:\hb32\bin\hbmk2.exe tBigNtst_dbfile.hbp -comp=mingw64 -cpp
-	c:\hb32\bin\hbmk2.exe tBigNtst_dbfile_memio.hbp -comp=mingw64 -cpp
-	c:\hb32\bin\hbmk2.exe tBigNtst_dbfile_memio_mt.hbp -comp=mingw64 -cpp
-	c:\hb32\bin\hbmk2.exe tBigNtst_dbfile_memio_mt_dyn_obj.hbp -comp=mingw64 -cpp 
+SET _HB_PATH=%HB_PATH%
+SET HB_PATH=c:\hb32\
+
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_array.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_array.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_array_assignv.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_array_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_array_mt.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_array_mt.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_array_mt_assignv.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_array_mt_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile.exe
+	
+	%HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_assignv.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_mt.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_mt.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_mt_assignv.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_mt_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_memio.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_memio_assignv.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_memio_dyn_obj.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_dyn_obj.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_memio_dyn_obj_assignv.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_dyn_obj_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_memio_mt.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_mt.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_memio_mt_assignv.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_mt_assignv.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_memio_mt_dyn_obj.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_mt_dyn_obj.exe
+	
+    %HB_PATH%bin\hbmk2.exe -cpp -comp=mingw64 tBigNtst_dbfile_memio_mt_dyn_obj_assignv.hbp
+REM    %HB_PATH%bin\upx.exe   tBigNtst_dbfile_memio_mt_dyn_obj_assignv.exe
+	
 	move /Y *.exe exe\mingw64
+SET HB_PATH=%_HB_PATH%
 SET PATH=%_PATH%
 
 

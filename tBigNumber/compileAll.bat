b179a22a80dc71acbda6d23d62135d7ad01599b8
marinaldo.jesus <marinaldo.jesus@91259c0f-3c9b-aaa7-74a1-b0048a0373d4>
2013-08-06 02:38:51 +0000

    Devido a Cota excedida no Google Code, tBigNumber agora está de casa nova GitHub. Em breve o link no leiame.txt

--- tBigNumber/compileAll.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo BATCH FILE FOR Harbour tBigNumber
-rem ============================================================================
-SET _PATH=%PATH%
-	call compile.bat
-	call compile64.bat
-	call compilearm.bat
-SET PATH=%_PATH%
--- tBigNumber/compileAllLib.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo BATCH FILE FOR Harbour tBigNumber
-rem ============================================================================
-SET _PATH=%PATH%
-	call compileLib.bat
-	call compileLib64.bat
-	call compileLibARM.bat
-SET PATH=%_PATH%
